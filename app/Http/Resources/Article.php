eb89f4c205f4dd8966ab9d9b6ee8f44f750d9c90
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2020-10-06 16:18:57 +0100

    Fix: replace Resource class with JsonResource

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class Article extends Resource
+class Article extends JsonResource
 {
     /**
      * Transform the resource into an array.
